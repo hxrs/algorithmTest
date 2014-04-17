f504e91225e07f8de9589ff5b052eb338063af86
hxrs <hxrs.jx@gmail.com>
2014-04-17 17:33:55 +0800

    MinDistanceOfThreeTuples-initilization

---
+++ src/main/java/com/github/hxrs/MinDistanceOfThreeTuples.java
@@ -0,0 +1,11 @@
+package com.github.hxrs;
+
+/**
+ * @author huanxiao
+ *
+ */
+public class MinDistanceOfThreeTuples {
+	public int[] findMinDistanceElments(int[] array1, int[] array2, int[] array3) {
+		return null;
+	}
+}
